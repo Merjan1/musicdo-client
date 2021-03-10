dae980ca592824acbc1943fa754c6e7f85ff6c58
Merjan1 <jvictor669@hotmail.com>
2021-03-10 10:13:35 -0300

    arrumado bugs com renderização de posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { Grid, CircularProgress } from '@material-ui/core' 
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
 import useStyles from "./Posts.styles";
 
 const Posts = () => {
-  const posts = useSelector((state) => state.posts);
+  const post = useSelector((state) => state.posts);
   const classes = useStyles();
 
-  console.log(posts);
+  console.log(post);
 
-  return (
-    !posts.lenght ? <CircularProgress/> :(
-      <Grid className={classes.container} container alignItems='stretch' spacing={3}>
-          {posts.map((post) =>(
-              <Grid key={post._id} item xs={12} sm={6} >
-                  <Post post={post}/>
-              </Grid>
-          ))}
-      </Grid>
-    )
+  return !post.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {post.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
